641696bc02d15d063d09d13e309c73c02d0fe520
yonibaru <theyonib@gmail.com>
2024-01-28 12:51:21 +0200

    makefile seems to be done, need opinions

--- main.c
+++ main.c
@@ -4,23 +4,25 @@
 
 int main(){
         int number1,number2;
-        printf("Enter the first number: ");
 
+        // We check if the user enters valid positive integers - 0 is not positive.
+        // This is to avoid errors within my functions.
         while(TRUE){
             scanf("%d", &number1);
             if(number1 > 0 && number1 < INT_MAX){
                 break;
             }
         }
-
-        printf("Enter the second number: ");
-
         while(TRUE){
             scanf("%d", &number2);
             if(number2 > 0 && number2 < INT_MAX){
                 break;
             }
-        }    
+        } 
+
+        // I'm aware the following is ineffective: Looping over the same thing 4 times.
+        // Unfortunately, we don't have Strings or ArrayLists to store the numbers.
+        // As per the text output examples, there could be massive numbers and creating an array of size 1-2 billion is just isn't feasible and will probably just cause our program to crash.
 
         printf("The Armstrong numbers are:");
 
@@ -38,6 +40,8 @@ int main(){
         }
         printf("\nThe Prime numbers are:");
 
+        //This is incredibily slow but I think we're supposed to use our required functions here and since they only need to recieve one arugment (int n) so I can't do anything to speed it up.
+    
         for(int i = number1; i<=number2; i++){
             if(isPrime(i)){
                 printf(" %d",i);
